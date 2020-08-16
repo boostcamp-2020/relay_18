57708e12c4f63c3a661c3a8a7c822d52c19f2ea1
saeeng <saeeng.op@gmail.com>
2020-08-16 19:29:07 +0900

    signin modify

--- backend/src/routes/user/service/signin.js
+++ backend/src/routes/user/service/signin.js
@@ -4,12 +4,13 @@ const createPWD = require('../../../modules/createPWD');
 const db = require('../../../sequelize/models/index');
 
 
+
 router.post('/', async (req, res, next) => {
     try {
-        let { user_id, user_pwd,user_name,user_school,user_address,user_birthday } = req.body;
-        if (!user_id || !user_pwd) throw Error();
-        let newPWD = await createPWD(user_pwd);
-        await db.user.create({id: user_id,password: newPWD,name: user_name,school: user_school,address: user_address,birthday: user_birthday})
+        let { id, password, name,school,address,birthday } = req.body;
+        if (!id || !password) throw Error();
+        let newPWD = await createPWD(password);
+        await db.user.create({id: id,password: newPWD,name: name, status:false, school: school,address: address,birthday: birthday})
         .then( result =>{
             res.status(201).json({ok:true, message:"회원가입을 축하드립니다"});
         })
@@ -21,5 +22,6 @@ router.post('/', async (req, res, next) => {
         res.status(400).json({ok:false, message:"제대로 작성하세여!!"});
     }
 });
+
 
 module.exports = router;
