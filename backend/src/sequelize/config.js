0a11c5180d10cdac2d48ef059a7585e80ae890f0
Yu Taek <lcpnine@gmail.com>
2020-08-16 15:53:52 +0900

    merge frontend pages

--- backend/src/sequelize/config.js
+++ backend/src/sequelize/config.js
@@ -1,16 +1,8 @@
-
-require('path');
-require('dotenv')
-
-
-
-
-//...
 module.exports = {
-  port: 3306,
-  username: "relay18",
-  password: "relay18!",
-  database: "boostcamp",
-  host:  "db-4n3l7.pub-cdb.ntruss.com",
-  dialect: "mysql",
-}
+      port: 3306,
+      username: "relay18",
+      password: "relay18!",
+      database: "boostcamp",
+      host:  " db-4n3l7.pub-cdb.ntruss.com",
+      dialect: "mysql",
+    }
